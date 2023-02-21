431c2946adef74d076f843287ba9c8478b5e61f3
ingyer-ks <ingyer.ks@gmail.com>
2023-02-21 22:04:38 +0900

    Updates

---
+++ component---src-pages-404-tsx-bd3ca2ca4849713576b8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_tailwind_mdx_blog=self.webpackChunkgatsby_starter_tailwind_mdx_blog||[]).push([[218],{6040:function(t,e,a){a.r(e);var n=a(7294),l=a(5441),s=a(3776);e.default=function(t){var e=t.data,a=t.location,i=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:i},n.createElement(s.pQ,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-bd3ca2ca4849713576b8.js.map
