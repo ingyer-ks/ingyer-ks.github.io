c7f2bb5402a5aaa1f3159a13b079d6ff01a95ffa
ingyer-ks <ingyer.ks@gmail.com>
2023-04-02 13:17:48 +0900

    uuid for pdfs

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -67,15 +67,25 @@ const BlogPostTemplate: React.FC<PageProps> = ({ data, location }) => {
             enableAnnotationAPIs: true,
           }
 
-          adobeDCView.previewFile(
-            {
-              content: {
-                location: { url: "../problems/" + encodeURI(title) + ".pdf" },
-              },
-              metaData: { fileName: title + ".pdf" },
+          adobeDCView.previewFile({
+            content: {
+              location: { url: "../problems/" + encodeURI(title) + ".pdf" },
             },
-            previewConfig
-          )
+            metaData: {
+              fileName: title + ".pdf",
+              id: (() => {
+                return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+                  /[xy]/g,
+                  function (c) {
+                    const r = (Math.random() * 16) | 0,
+                      v = c == "x" ? r : (r & 0x3) | 0x8
+                    return v.toString(16)
+                  }
+                )
+              })(),
+            },
+            previewConfig,
+          })
         })
       }
       if (!script) {
