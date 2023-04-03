469b3ada009e41aacd37dfacb569b42a367c7336
ingyer ks <ingyer.ks@gmail.com>
2023-04-03 20:47:25 +0900

    working on 2022-9-national-ee

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,6 +104,34 @@ module.exports = {
         clarity_project_id: "gct6axljb9",
         enable_on_dev_env: true
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-MJQQSCBQDN",
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://www.googletagmanager.com",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
   ],
 }
