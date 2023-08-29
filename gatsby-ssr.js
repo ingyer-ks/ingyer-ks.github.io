e2b10c9177cfcb8ce64bf622e849c520258f6dc9
ingyer ks <ingyer.ks@gmail.com>
2023-08-29 17:43:23 +0900

    2021-9-seoul-comm

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,16 +3,20 @@ import { wrapRootElement as wrap } from "./wrapRootElement"
 import "@fontsource/noto-sans-kr"
 
 const HeadComponents = [
-  <><link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
-    integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
-    crossOrigin="anonymous"
-  ></link>
+  <>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+      integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+      crossOrigin="anonymous"
+    ></link>
     <link rel="favicon" href="./favicon.ico"></link>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9535714360512834"
-      crossorigin="anonymous"></script>
-  </>
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9535714360512834"
+      crossOrigin="anonymous"
+    ></script>
+  </>,
 ]
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
